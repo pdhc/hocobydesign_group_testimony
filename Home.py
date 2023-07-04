61045caddc9e4d5432ea10ba2396b0b036e77f3a
hiruyhadgu <65324470+hiruyhadgu@users.noreply.github.com>
2023-07-04 17:15:36 -0400

    Changes to be committed: 	modified:   Home.py 	new file:   groups/ionhoco.jpeg 	new file:   pages/12_Dynamic_Neighborhoods.py

--- Home.py
+++ Home.py
@@ -1,5 +1,10 @@
 import streamlit as st
+st.header("HoCo By Design Community Testimony")
 
+st.markdown("""The Howard County Council is delebrating HoCo By Design - the once-every-ten-year General Plan.
+It is imperative that community members share their concerns about the plan. Accordingly, community members have put
+together the following testimony, which has been endorsed by the following organizations.
+            """)
 
 st.markdown("""
 The following organizations and groups are signatories to the following testimony. Organizations who would
@@ -22,41 +27,27 @@ st.markdown(contact_form, unsafe_allow_html=True)
 
 st.write('----')
 
-expander1 = st.expander("**Community Organizations Supporting Testimony**")
+st.header("Community Organizations Supporting Testimony")
 
-with expander1:
-    st.write('---')
-    hcca,n4b = st.columns(2)
-    with hcca:
-        st.image("groups/hcca.jpg")
-    with n4b:
-        st.image("groups/n4b.jpeg")
-    st.write('---')
-    pdhc,sca = st.columns(2)
-    with pdhc:
-        st.image("groups/pdhc.jpeg")
-    with sca:
-        st.image("groups/sca.jpg")
-    st.write('---')
-    tpv,sca = st.columns(2)
-    with tpv:
-        st.image("groups/tpv.png")
-
-    st.write('---')
-        
-
-st.write('----------------------------------------------------------------')
-
-
-st.expander("HOA's Supporting Testimony")
-
-st.write('----------------------------------------------------------------')
-
-st.expander("PTA's Supporting Testimony")
-
-st.write('----------------------------------------------------------------')
-
-st.expander("Individuals Supporting Testimony")
+st.write('---')
+hcca,n4b = st.columns(2)
+with hcca:
+    st.image("groups/hcca.jpg")
+with n4b:
+    st.image("groups/n4b.jpeg")
+st.write('---')
+pdhc,sca = st.columns(2)
+with pdhc:
+    st.image("groups/pdhc.jpeg")
+with sca:
+    st.image("groups/sca.jpg")
+st.write('---')
+tpv,ionhoco = st.columns(2)
+with tpv:
+    st.image("groups/tpv.png")
+with ionhoco:
+    st.image("groups/ionhoco.jpeg")
+st.write('---')
 
 # Use Local CSS File
 def local_css(file_name):
---
+++ pages/12_Dynamic_Neighborhoods.py
@@ -0,0 +1,132 @@
+import streamlit as st
+
+st.markdown("""
+
+### Dynamic Neighborhoods: Maintaining and Supporting Vibrant Living
+
+###### Lack of Consideration for Schools, Hospitals and Other Infrastructure included in Adequate Public Facilities Ordinance (APFO)
+
+HoCo by Design defines “Dynamic Neighborhoods” as “places that provide an exceptional quality of life, attainable and 
+diverse housing options, and opportunities for all to thrive through access and connection to neighborhood amenities, such as 
+community centers, parks, schools, and jobs.” (Page 118 of PDF). However, while the General Plan may pay lip service to these 
+other crucial community needs aside from just housing also being essential components of “Dynamic Neighborhoods”, it is clear 
+that those other needs like schools, which are the main thing that brings people to Howard County in the first place, are not taking 
+priority when it comes to evaluating what produces Dynamic Neighborhoods, as evidenced by their exclusion from this Chapter altogether, 
+and the recommendation that APFO regulations ensuring quality public facilities and sustainable growth be clawed back, and the false suggestion 
+that this would somehow produce more equitable results. (Pages 119 and 142 of PDF). If anything, current APFO regulations are far too weak, and need 
+to be strengthened to solve our inequitable school overcrowding and other infrastructure-related crises–all of which absolutely go into making 
+up equitable and Dynamic Neighborhoods! While APFO currently includes protections against school overcrowding beyond a certain point, hospitals 
+and other healthcare facilities also need to be added to APFO considerations, as they currently are in Montgomery County, to prevent hospital overcrowding 
+and bed shortages, which could become deadly as our senior population in the County continues to grow, and our sole hospital in the County (Howard County General Hospital, 
+owned by Hopkins) regularly remains over-capacity.
+
+Chapter 6 of HoCo by Design states one of its goals is to “​​Assess existing community facilities and the neighborhoods that they serve, 
+and upgrade or retrofit as needed to support changing neighborhood needs.” The County needs to walk the walk when it comes to this, not just 
+talk the talk. This would mean giving long-neglected communities like Elkridge the investments in local infrastructure and amenities that they desperately 
+need, like sidewalks, a community center, and a High School, though HoCo by Design does not currently provide for these necessary upgrades that many other parts of the County already have.
+
+###### Flawed Assumptions Related to Housing Affordability and its Relationship with Density and Supply
+
+The Dynamic Neighborhoods chapter takes note of the community desire to make housing more affordable in Howard County. 
+However, the County seems to be approaching affordable housing in completely the wrong manner. First off, the County is relying 
+on the false assumption that increasing housing density and supply necessarily means it will become more affordable; in reality, when not 
+accompanied by any other measures that mandate affordability, this is often not the case, and increasing density and supply just means developers can 
+now build more high-priced units than before. (Pages 119 and 123 of PDF). Take, for example, the new Long Gate Overlook townhouses, which are selling in 
+the \\$600-700k ranges, which is significantly more expensive than many single family homes, despite being denser than them. In fact, the General Plan undermines its 
+own argument that housing unaffordability is a result of housing supply failing to keep up with demand, when it admits on Page 125 that “In Howard County, housing demand 
+tends to follow supply [rather than the other way around].” Again, the idea that increasing housing density and supply automatically equates to more affordability is a misleading myth, 
+put out by the developer lobby to justify zoning deregulation that will increase their profits. 
+
+###### Flawed Assumptions Related to Housing Affordability and its Relationship with Diverse Housing Options and “Missing Middle” Housing
+
+Another misleading myth that the General Plan currently is relying on to try to make housing more 
+affordable is that diversification of housing options will necessarily equate to more affordability. (Pages 119, 127 and 137 of PDF). Similar to 
+the example used earlier about townhouses and Long Gate Overlook, the same principle can apply for other housing types, too, such as condos, which can 
+likewise sometimes be priced more expensive than single family homes are. Chapter 6 of the General Plan consistently harps on the need to add more “Missing Middle” 
+housing to the County, but the General Plan is also sure to clarify that “missing middle” is referring to the size of the housing, not the price of the housing! (“Missing Middle” defined 
+as “a range of small- to medium-size home choices that are available at different price points” on Page 127 of PDF). But again, higher density or smaller housing does not necessarily equate 
+to more affordable housing, as there is nothing stopping developers from still charging high rates for the smaller or denser housing units. 
+
+###### Flawed Conclusion about New Residential Developments Automatically Net Increasing County Tax Revenue
+
+In Chapter 6 of HoCo by Design, they echo another oft-repeated, yet false, talking point about new residential development always net increasing the County tax budget, 
+by net adding more tax revenue for the County through the new residents than the expenses that are generated for the County by these new residents and developments. (Page 137 of PDF). 
+While it would be convenient if this were true, since if it was, then in theory many of our County’s problems resulting from budgetary shortages could be solved simply just by building more 
+houses, unfortunately in reality both the data and common sense proves this statement to be unfounded. In fact, the polar opposite is true: adding new housing units (and therefore residents) 
+adds much more expenses to the County through new students generated in the school system, increased burdens on traffic and roads and other infrastructure through more cars on the road, 
+increased demand on our already-constrained sole hospital in the County, etc. than the new residents generate in tax revenue for the County. Decades of operating under this untrue assumption 
+and building non-stop without charging developers the appropriate infrastructure impact fees for new developments has led to chronic overcrowding and underfunding of our public schools, a lack 
+of public transportation, and a hospital that is regularly in Code Red due to being constantly over-capacity. The erroneous assumptions and data that the County has been using to arrive at this 
+false conclusion that simply increasing residential development net increases the County tax budget are debunked more in-depth here. If the County wants this statement to actually be true, and for 
+residential development to truly pay for itself, then impact and mitigation fees charged to developers on new housing units must be greatly increased; otherwise, the tax burden on residents will 
+continue to increase, while services and quality of life still decline overall.
+
+###### Flawed, Misleading, Easily Exploitable Definition of “Affordable Housing”
+
+On top of the flawed assumptions the General Plan makes about what creates more affordable housing, the way it defines “affordable housing” in the first 
+place is also highly problematic. The General Plan says affordable housing can “include both income-restricted housing, as well as attainably priced market-rate 
+housing”. (Page 122 of PDF). The General Plan goes on to define this “attainable housing” as follows: “An attainably priced home is one that does not create cost burdens 
+for the family living there and is generally affordable without a subsidy. According to the US Department of Housing and Urban Development (HUD), a home is affordable if 
+the occupant is paying no more than 30% of gross income for housing costs, including utilities. Based on this definition, a household that makes \\$73,000 per year could probably afford a monthly 
+rent of \\$1,825 and a mortgage on a home priced at \\$200,000–\\$250,000.” This definition of affordable/attainable housing is very concerning, because unlike income-restricted 
+affordable housing, what is considered “affordable” or “attainable” is all relative to any given person’s income level. This creates a subjective definition of affordable and 
+attainable housing, which can be taken advantage of by even those with very high income levels, rather than an objective definition of affordable and attainable housing which 
+can only be applied to people of low or moderate incomes who truly need the lower-cost housing. So just how “affordable” will much of this so-called affordable housing be? And “affordable” for who?
+
+###### Adequately Addressing the Fee in Lieu Loophole Abused by Developers to Evade Affordable Housing Requirements and Segregate Communities
+
+If the County is truly serious about wanting to increase affordable housing, it should eliminate the fee in lieu option which 
+allows developers to evade affordable housing requirements by paying a small fee that is a tiny fraction of the price of actually building the 
+affordable housing units. Instead, the General Plan unfortunately defends this obvious loophole abused to increase developer profits at the expense 
+of building more affordable housing units. (Page 141 of PDF). The fee in lieu option, along with Chapter 6 of the General Plan’s stated Affordability Target, 
+present a conflict with another one of the General Plan’s stated goals of effectively desegregating the County by more evenly dispersing affordable housing 
+throughout all parts of the County. (Page 141 of PDF). The General Plan’s stated Affordability Target is “The greater of at least 15% of all net new housing units 
+should be available to households making less than 60% of AMI each year”. (Page 141 of PDF). Since the affordability target only requires 15% of all net new housing units to be affordable, 
+rather than just 15% of all new housing units period to be affordable, then the fee in lieu option will allow developers to still evade building affordable housing units in certain sections 
+of the County, while clustering any affordable housing units they are required to build in other parts of the County, effectively continuing the trend of increasing segregation in Columbia and the County at large.
+
+###### Lack of Rent Stabilization and Rent Control Protections for Renters in Apartments, ADUs, Duplexes, Triplexes, and Quadplexes
+
+Chapter 6 of the General Plan makes another dangerous and untrue assumption: that rental units are necessarily more affordable to low-income people 
+than non-rental units are. Although people of low income may have greater access to renting because it may not require a large down payment or mortgage, 
+without proper protections for renters, the monthly price to rent can sometimes be just as much, if not more, as the price per month that non-renters may have to pay for their housing. 
+On top of this, there is nothing stopping landlords from increasing rents by drastic amounts, and doing other predatory landlord/management practices. Indeed, in some rental units in 
+Howard County, people’s rents are being increased by more than 30% in a single year! Without some form of rent control or rent stabilization, similar to the 3% annual rent increase cap 
+which just passed in Prince George’s County, solely increasing the number of rental units will likely just lead to more vulnerable renters being exploited and rent-gouged by unregulated 
+and profit-driven landlords, which more and more often these days are heartless and notoriously cruel real estate management companies, such as Buzzoto and Blackstone. This same issue also 
+applies to accessory dwelling units (ADUs), and duplexes, triplexes, quadplexes, etc., which essentially just make it easier for people and companies to be landlords, and are susceptible to 
+the same predatory rent-gouging practices as regular apartments are. Red flags should further be triggered by the fact that it is developer attorneys and lobbyists themselves who are actively 
+pushing for this unregulated shift towards ADUs, without any adequate rent stabilization protections for potential renters. Of course, ADUs, duplexes, triplexes, and other forms of “missing middle” housing should 
+still have to comply with APFO regulations, too, although the General Plan currently seems open to exempting them. (Page 136 of PDF).
+
+Chapter 6 of the General Plan, however, does not argue that rent stabilization is needed, and instead argues that rents increasing are simply a 
+result of not enough of a supply of rental units. (Page 125 of PDF). This supply and demand argument is just as erroneous in the context of rent gouging as it 
+is in the context of rising housing prices in general supposedly being the result of a housing supply and demand mismatch. Both arguments identify real problems 
+that are actually largely caused by developers and/or landlords themselves, and then propose false bait-and-switch solutions that really just increase developer and/or 
+landlord profits. In fact, in complete contradiction to the claim that the County has not been building enough rental units, Graph 6-3 on Page 126 actually shows that the 
+County built far more rental units from 2010-2019 than they built any other type of housing units, including townhomes and single family homes, during that same period. 
+Similarly, the home ownership rate has decreased from 77% to 72% in Howard County from 2005 to 2018. (Page 143 of PDF). But again, without renter protections like rent 
+stabilization, no amount of new rental units being built will adequately address rising and unaffordable rent prices from predatory and unregulated landlords. And redevelopments 
+that add rental units without adequate rent stabilization and other renter protections may result in gentrification and displacement of existing residents.
+
+#### Recommended Changes:
+
+* MANDATES (not “incentives”/tax breaks) on affordable housing, senior housing, homeless preference set-aside units, and housing for those with disabilities (more regulation, rather than deregulation)
+* Get rid of fee in lieu option that allows developers to evade affordable housing requirements
+* For MIHU program, set MIHU requirement to 25% of all units (as opposed to the current requirement range of just 10-25%)
+* Only use income-restricted housing types for affordable housing classifications; don’t include “Attainable housing”/attainably-priced market rate housing as defined in the General Plan as counting as Affordable Housing
+* Add requirements/mandates for Extremely Low Income Housing Units, in addition to just Low Income and Moderate Income Housing Units
+* Stronger APFO protections (including adding hospital and healthcare facilities to APFO) to ensure housing and infrastructure are developed 
+at pace with one another in an equitable way. No exceptions to APFO and stormwater management and sewage and septic regulations, whether for ADUs, 
+housing for people with disabilities, senior housing, affordable housing, duplexes, triplexes, or any other form of housing–ALL residents deserve quality and safe infrastructure!
+* Strong rent control/rent stabilization policies (no more than 3% rent increases per year), for non-profit and for-profit developments. 
+The County should be proactive when it comes to monitoring and inspecting landlord compliance with property regulations, rather than waiting to receive a complaint.
+* Greatly increase school surcharge fees and other impact and mitigation fees for developers
+* Don’t expand the PSA, and protect the unique character of the Rural West by not allowing further development in that region; preserve the forests and farmland and quiet, 
+peaceful setting of the Rural West, including by limiting agribusiness and commercial uses of land in residential areas e.g. no breweries. Affordable housing for agricultural 
+workers should be OFF SITE from the farms/workplaces (on-site housing for agricultural workers has a long and ongoing history of being easily and coercively abused by employers)--Pages 119, 120, 123, 137, and 144 of PDF
+* Enable and encourage “aging in place” options and accommodations for seniors in Howard County who prefer to age in place, rather than to move somewhere else
+* Greater investment in publicly-owned, rather than privately-owned, quality housing
+
+
+""")
